b2160f0a18f17cae37639306e48b1a5f25f7abec
mark7389 <mark.soliman73@gmail.com>
2017-10-21 09:31:59 -0400

    intial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+var PORT = process.env.PORT || 3000;
+
+//boiler plate
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/",function(req, res){
+
+	res.send("some text");
+
+});
+
+
+
+
+app.listen(PORT, function(){
+
+	console.log("Listening on perot %s", PORT);
+});
